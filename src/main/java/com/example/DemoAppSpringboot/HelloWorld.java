528619c8a5bb215d48d9d87bfbd9b17fbfa01156
akhildornala <32592689+akhildornala@users.noreply.github.com>
2025-05-25 21:05:14 -0700

    Update HelloWorld.java

--- src/main/java/com/example/DemoAppSpringboot/HelloWorld.java
+++ src/main/java/com/example/DemoAppSpringboot/HelloWorld.java
@@ -11,5 +11,6 @@ public class HelloWorld {
     @GetMapping("welcome/{name}")
     public String helloWorld(@PathVariable("name") String name) {
         return "Hello World " + name;
+        
     }
 }
